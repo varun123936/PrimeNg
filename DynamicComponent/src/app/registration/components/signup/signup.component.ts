f6646b7b2d27070aa78fdaaeecc4f31fb1f24a83
varun123936 <55535912+varun123936@users.noreply.github.com>
2026-03-23 13:29:15 +0000

    Signup form with Reactive Forms + validation (name, address, phone, email, etc.) and PrimeNG inputs.

--- DynamicComponent/src/app/registration/components/signup/signup.component.ts
+++ DynamicComponent/src/app/registration/components/signup/signup.component.ts
@@ -1,8 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {}
+export class SignupComponent {
+  submitted = false;
+
+  form = this.fb.group({
+    fullName: ['', [Validators.required, Validators.minLength(2)]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]],
+    address: ['', [Validators.required, Validators.minLength(5)]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required]]
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  submit(): void {
+    this.submitted = true;
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    // Hook for signup logic (json-server) will be added in the next feature.
+  }
+}
--- DynamicComponent/src/app/registration/registration.module.ts
+++ DynamicComponent/src/app/registration/registration.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RegistrationRoutingModule } from './registration-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { PasswordModule } from 'primeng/password';
 
 @NgModule({
   declarations: [LoginComponent, SignupComponent],
-  imports: [CommonModule, RegistrationRoutingModule]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RegistrationRoutingModule,
+    ButtonModule,
+    InputTextModule,
+    InputTextareaModule,
+    PasswordModule
+  ]
 })
 export class RegistrationModule {}

b1d1dc91e86bdcd9099d680f951870eef852b5a2
CodexSandboxOffline <CodexSandboxOffline@hdc3-l-5324xsc.dir.svc.accenture.com>
2026-03-25 20:40:21 +0530

    Remove global error handlers

--- DynamicComponent/src/app/app.module.ts
+++ DynamicComponent/src/app/app.module.ts
@@ -1,7 +1,7 @@
-import { ErrorHandler, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 
@@ -9,8 +9,6 @@ import { AppComponent } from './app.component';
 import { DynamicFormModule } from './dynamic-form/dynamic-form.module';
 import { AppRoutingModule } from './app-routing.module';
 import { ShopModule } from './shop/shop.module';
-import { GlobalErrorHandler } from './core/global-error-handler';
-import { HttpErrorInterceptor } from './core/http-error.interceptor';
 
 @NgModule({
   declarations: [
@@ -26,9 +24,7 @@ import { HttpErrorInterceptor } from './core/http-error.interceptor';
     ShopModule
   ],
   providers: [
-    MessageService,
-    { provide: ErrorHandler, useClass: GlobalErrorHandler },
-    { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true }
+    MessageService
   ],
   bootstrap: [AppComponent]
 })

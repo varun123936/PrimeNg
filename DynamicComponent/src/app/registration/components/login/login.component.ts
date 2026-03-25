696749e301f2d4eb57df129accd1c9acb7d0a3c5
varun123936 <55535912+varun123936@users.noreply.github.com>
2026-03-25 05:36:25 +0000

    Login form with Reactive Forms + validation + PrimeNG inputs

--- DynamicComponent/src/app/registration/components/login/login.component.ts
+++ DynamicComponent/src/app/registration/components/login/login.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {}
+export class LoginComponent {
+  submitted = false;
+
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  submit(): void {
+    this.submitted = true;
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    // Hook for login logic (mock auth) will be added in the next feature.
+  }
+}
